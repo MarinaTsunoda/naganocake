e4f3eb32a24a98adc4433d5782ee9c723e89d9ba
Marina Tsunoda <122651621+MarinaTsunoda@users.noreply.github.com>
2023-03-13 15:38:23 +0000

    Make view(admin/sign_in,admin/genre,admin/items/new)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,17 @@
 class Admin::ItemsController < ApplicationController
+  before_action :set_genres, only: [:new]
   def index
+    @items=Item.all
   end
 
   def new
+    @item=Item.new
   end
 
   def create
+    @item=Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -15,5 +21,12 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre, :price, :is_active)
+  end
+  def set_genres
+    @genres=Genre.all.pluck(:name, :id)
   end
 end
