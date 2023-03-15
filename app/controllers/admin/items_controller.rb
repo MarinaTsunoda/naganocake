f2faa575862df23d1275bb837ab48e90efbf81b2
Marina Tsunoda <122651621+MarinaTsunoda@users.noreply.github.com>
2023-03-15 04:45:03 +0000

    Fix view(items/show,items/edit,items/index)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
   
   def index
-    @items=Item.all
+    @items=Item.page(params[:page])
   end
 
   def new
@@ -16,12 +16,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item=Item.find(params[:id])
   end
 
   def edit
+    @item=Item.find(params[:id])
+    @genres=Genre.all
   end
 
   def update
+    @item=Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_items_path
   end
   private
   def item_params
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,10 @@ class Admin::OrdersController < ApplicationController
   def show
   end
 
+ def index
+   @orders=Order.page(params[:page])
+ end
+ 
   def update
   end
 end
