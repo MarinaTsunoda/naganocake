21d3ecc9d32cfe61482aac4e54df47c7f8197b3f
Marina Tsunoda <122651621+MarinaTsunoda@users.noreply.github.com>
2023-03-17 15:09:10 +0000

    Fix items/new_layout

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,7 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order=Order.find(params[:id])
+    @order_detail=@order.Order_detail.find(params[:id])
+    @order_detail.update
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order=Order.find(params[:id])
+    @order_details=Order_detail(@order.id)
   end
 
  def index
@@ -7,5 +9,7 @@ class Admin::OrdersController < ApplicationController
  end
  
   def update
+    @order=Order.find(params[:id])
+    @order.update
   end
 end
