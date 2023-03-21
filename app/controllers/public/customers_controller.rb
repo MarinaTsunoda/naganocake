277ea0c3cc6ab56c06edd8d75165cdfb4b237719
Marina Tsunoda <122651621+MarinaTsunoda@users.noreply.github.com>
2023-03-21 15:30:12 +0000

    Fix views/addresses/index,edit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -17,5 +17,11 @@ class Public::CustomersController < ApplicationController
   end
 
   def withdraw
+    current_customer.update(is_deleted:true)
+    reset_session
+    redirect_to root_path, notice: 'Successfully withdraw from Ecommerce'
+  end
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
 end
